0541fc767c9c4e8e3a701b656a45f24bb233f118
ajblocker <ajblocke@gmail.com>
2026-03-22 12:32:44 -0400

    Implemented security measures

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import { logInLimiter } from '../middleware/rateLimiter.js';
 import { signUpHandler, logInHandler } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/signup', signUpHandler);
-router.post('/login', logInHandler);
+router.post('/login', logInLimiter, logInHandler);
 
 export default router;
